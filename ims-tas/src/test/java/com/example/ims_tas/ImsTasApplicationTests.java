2a8f7387f6c9c91a89d27c853011c0df7f21cf3d
ram-taterao-dhumal-ext <ram.taterao-dhumal-ext@hpe.com>
2025-09-18 13:54:11 +0530

    added maven project

---
+++ ims-tas/src/test/java/com/example/ims_tas/ImsTasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ims_tas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsTasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
